482edb099dbff6b40617e39b8840707507b00c52
Miguel Ercolino <mercolino@gmail.com>
2015-02-25 15:24:32 +0100

    Modified README and adding features to installation script

--- README.txt
+++ README.txt
@@ -1,1 +1,11 @@
-Initial README
+MHN Kippo Graphs
+================
+
+This is a set of scripts written in python and bash shell that add the capability to the ThreatStream Modern Honey Network (MHN)
+to generate statistics about the kippo sensors installed on the network.
+
+For instructions on how install this please visit: http://itandsecuritystuffs.wordpress.com/
+
+If you have any issues please report them using Github: https://github.com/mercolino/mhn_kippo_graphs/issues
+
+Link to ThreatStream MHN code: https://github.com/threatstream/mhn
--- install_kippo_graph.sh
+++ install_kippo_graph.sh
@@ -2,26 +2,36 @@
 
 set -e
 
+# Install MySQL and matplotlib dependencies
 sudo apt-get update
 sudo apt-get install -y mysql-server libpng12-devel libfreetype6-dev libxft-dev
 
-MHN_HOME=`dirname $0`/..
+# Getting MHN directory
+MHN_HOME=`dirname $0`/../..
 cd $MHN_HOME
 MHN_HOME=`pwd`
 
+# Installing pymysql numpy matplotlib and PILLOW
 . env/bin/activate
 pip install pymysql numpy matplotlib PILLOW
 
+# Downloading the sql file to create tables on the databases
 cd $MHN_HOME/scripts
 sudo wget https://github.com/threatstream/kippo/raw/master/doc/sql/mysql.sql -O kippo_mysql.sql
 
+# Creating the directory inside mhn server app
 mkdir $MHN_HOME/server/mhn/static/img/kippo_graphs
 chown www-data:www-data $MHN_HOME/server/mhn/static/img/kippo_graphs
 
+# Adding the kippo with mysql support to mhn app
+sed -i "150i\            'Ubuntu - Kippo with MySQL support': path.abspath('../scripts/mhn_kippo_graphs/deploy_kippo_MySQL_support.sh')," $MHN_HOME/server/mhn/__init__.py
 
-sed -i "150i\            'Ubuntu - Suricata': path.abspath('../scripts/mhn_kippo_graphs/deploy_kippo_MySQL_support.sh')," /opt/mhn/server/mhn/__init__.py
-
+# Reloading the mhn service
 supervisorctl restart mhn-uwsgi
 
+# Creating CronTab to run the generating graph script every 5 minutes
+crontab -l | { cat; echo "*/5 * * * * python $MHN_HOME/scripts/mhn_kippo_graphs/kippo_generate_graphs.py"; } | crontab -
+
+# Remember to run this script
 echo "Remember, each time a kippo sensor with MySQL support is installed you should run the following command:"
 echo "sudo python /opt/mhn/scripts/mhn_kippo_graphs/kippo_update_mysql.py"
